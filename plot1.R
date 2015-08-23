b0c9735bbd81198ca7281a523ab17e15ae480f1a
briahnna123 <briahnna123@users.noreply.github.com>
2015-08-22 19:23:12 -0700

    Changes

--- plot1.R
+++ plot1.R
@@ -23,18 +23,18 @@ NEI <- readRDS("summarySCC_PM25.rds") # Bring in data
 # Step 2: "Clean" the Data
 data <- with(NEI, aggregate(Emissions, by = list(year), sum))
 # Use with() to combine/sum the Emissions by year in seperate list
-names(data)[1] <- "Year"
-names(data)[2] <- "Emissions"
+names(data)[1] <- "Year" # Rename the Column
+names(data)[2] <- "Emissions" # Rename the Column
                                         
 # Step 3: Create the plot
-png(filename="plot1.R", width=490, height=490, units="px")
+png(filename="plot1.R", width=490, height=490, units="px") # Set pic format
 par(mar=c(5, 5, 4.1, 3)) # Adjust the Margin sizes
 plot(data, type= "b", pch= 15, col= "palevioletred1", yaxt="n", xaxt="n",
      xlab="Years", ylab= "Emissions (Tons)")
 title(main="Plot of Total Emmisions by Years", col.main="palevioletred1")
-axis(2, at=c(3E06, 4E06, 5E06, 6E06, 7E06, 8E06), cex.axis=.75)
-axis(1, at= c(1999, 2002, 2005, 2008))
-dev.off()
+axis(2, at=c(3E06, 4E06, 5E06, 6E06, 7E06, 8E06), cex.axis=.75) # Add Y axis labels
+axis(1, at= c(1999, 2002, 2005, 2008), cex.axis=.90) # Add X axis Labels
+dev.off() # Close Graphics and Show Plot WD
 
 
 
