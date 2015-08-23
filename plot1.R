9ca1659173bd60170d5000eaf7570af437667201
briahnna123 <briahnna123@users.noreply.github.com>
2015-08-22 19:15:53 -0700

    Changes

--- plot1.R
+++ plot1.R
@@ -21,8 +21,6 @@ NEI <- readRDS("summarySCC_PM25.rds") # Bring in data
 
 
 # Step 2: "Clean" the Data
-NEI$year <- as.factor(NEI$year) # Make year into factor
-
 data <- with(NEI, aggregate(Emissions, by = list(year), sum))
 # Use with() to combine/sum the Emissions by year in seperate list
 names(data)[1] <- "Year"
@@ -30,15 +28,13 @@ names(data)[2] <- "Emissions"
                                         
 # Step 3: Create the plot
 png(filename="plot1.R", width=490, height=490, units="px")
-
-with(data, plot(Year, Emissions, type="b", xlab="Years", ylab= "Emissions (Tons)"))
-title(main="Scatter Plot of Emmisions over Years")
-
-plot(data, type= "b", pch= 15, xlab="Years", ylab= "Emissions (Tons)")
-
-title(main="Scatter Plot of Emmisions over Years")
+par(mar=c(5, 5, 4.1, 3)) # Adjust the Margin sizes
+plot(data, type= "b", pch= 15, col= "palevioletred1", yaxt="n", xaxt="n",
+     xlab="Years", ylab= "Emissions (Tons)")
+title(main="Plot of Total Emmisions by Years", col.main="palevioletred1")
+axis(2, at=c(3E06, 4E06, 5E06, 6E06, 7E06, 8E06), cex.axis=.75)
+axis(1, at= c(1999, 2002, 2005, 2008))
 dev.off()
-
 
 
 
