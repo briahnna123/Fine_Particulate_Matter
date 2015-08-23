4c9366fba34fb03bdb20d6244707d0d4aca2a262
briahnna123 <briahnna123@users.noreply.github.com>
2015-08-22 23:13:24 -0700

    Third Code

---
+++ plot3.R
@@ -0,0 +1,36 @@
+# Coursera Exploratory Data Analysis Assignment
+# Plot 3: 
+
+# Directions 
+
+# You must address the following questions and tasks in your exploratory analysis.
+# For each question/task you will need to make a single plot. Unless specified, 
+# you can use any plotting system in R to make your plot.
+
+# Of the four types of sources indicated by the type (point, nonpoint, onroad, 
+# nonroad) variable, which of these four sources have seen decreases in emissions
+# from 1999–2008 for Baltimore City? 
+
+# Which have seen increases in emissions from 1999–2008? Use the ggplot2 
+# plotting system to make a plot answer this question.
+
+# Step 1: Set WD and Bring in data
+# Note: The rds file stores, connects, and saves R objects
+setwd("~/Desktop")
+NEI <- readRDS("summarySCC_PM25.rds") # Bring in data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
